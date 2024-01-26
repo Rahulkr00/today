987e9c7cd6bdb41f723f3262545769593d9e369e
Rahulkr00 <care.rahulsingh07022003@gmail.com>
2024-01-26 15:52:48 +0530

    i commited

---
+++ king.java
@@ -0,0 +1,13 @@
+pdjsjij]'lk;.////////aa turn in yto yhejk
+
+
+
+
+shdkghskghdshkhgkjhduhhdghahuoP{{  
+jflkjdfkljlkjglkajituriu ]];;'//'////df void sjljdlkajlja
+hasdhkajglajjaglakjglkajglagkhlgkhljajbagbsjlhg
+kjfklkla
+
+
+sgjajkhglkajg;lajkgljlkghlmng
+gkladkhglkjglkjlgjljk
